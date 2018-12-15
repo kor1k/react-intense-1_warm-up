15f34d6e60b5dc79f7a45c19054a7e5026b74f99
Kostya <koortttt@gmail.com>
2018-12-15 18:54:54 +0200

    added images. " what is it "props" "

--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Hello() {
+    let phrase = "World";
+    let symbol = "!";
+    // let userName = "Kostya Yelskiy";
+    let teacherName = "Ilya";
+    let cityWhereIsUser = "Kyiv";
+    return (
+        <h1>{123123 * 2} Hello {phrase} {symbol} Have a nice day {teacherName}. Which weather is
+            in {cityWhereIsUser}? </h1>
+    )
+}
+
+function SayFullName(props) {
+    return (
+        <div>
+            <h1>My name is {props.name}, surname - {props.surname}</h1>
+            <a href="{props.link}">Link on my profile.</a>
+        </div>
+    )
+}
+
+ReactDOM.render(<SayFullName name="Kostya" surname="Yelskiy"
+                             link="https://github.com/kor1k"/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
